cb67322bcbcd4b7e70061127271a66e1c7ba75a9
hemant <hk443957@gmail.com>
2023-08-07 22:24:35 +0530

    fronted regiter componenet complete going to setup backend and complete register api

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Register from './pages/Register'
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/register' element={<Register></Register>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App;
---
+++ client/src/pages/Register/index.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect } from 'react';
+import { Button, Form, Input, message } from 'antd';
+// import { registerUser } from '../../apicalls/usersApi';
+// import { useNavigate } from 'react-router-dom';
+// import { useDispatch } from 'react-redux';
+// import { setLoader } from '../../redux/loaderSlice';
+const index = () => {
+    // const navigate = useNavigate();
+    // const dispatch = useDispatch();
+    const onFinish = async (values) => {
+        try {
+            dispatch(setLoader(true));
+            const response = await registerUser(values);
+            dispatch(setLoader(false));
+            if (response.success) {
+                message.success('user registered successfully');
+                navigate("/login");
+            } else {
+                throw new Error(response.message);
+            }
+        } catch (error) {
+            message.error(error.message);
+        }
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    return (
+        <div className='  bg-slate-500 rounded-lg h-screen w-full  flex items-center justify-center '>
+            <div className=' flex items-center justify-center flex-col pr-10 bg-slate-400 '>
+                <div className='text-3xl mb-4'>Connect Me</div>
+                <div className=''>
+                    <Form
+                        name="register"
+                        labelCol={{ span: 8 }}
+                        wrapperCol={{ span: 16 }}
+                        className='lg:min-w-[600px] md:ml-1'
+                        // initialValues={{ remember: true }}
+                        onFinish={onFinish}
+                        onFinishFailed={onFinishFailed}
+                        autoComplete="off"
+                    >
+                        <Form.Item
+                            label="Name"
+                            name="name"
+                            rules={[{ required: true, message: 'Please enter you name!' }]}
+                        >
+                            <Input type='text' />
+                        </Form.Item>
+                        <Form.Item
+                            label="Username"
+                            name="username"
+                            rules={[{ required: true, message: 'Please input your username!' }]}
+                        >
+                            <Input type='text' />
+                        </Form.Item>
+                        <Form.Item
+                            label="Email"
+                            name="email"
+                            rules={[{ required: true, message: 'Please input your Email!' }]}
+                        >
+                            <Input type='email' />
+                        </Form.Item>
+
+                        <Form.Item
+                            label="password"
+                            name="password"
+                            rules={[{ required: true, message: 'Please input your password!' }]}
+                        >
+                            <Input.Password />
+                        </Form.Item>
+                        <Form.Item wrapperCol={{ offset: 13, span: 16 }}>
+                            <Button type="primary" htmlType="submit" className='rounded-md'>
+                                Submit
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </div>
+                <div className='cursor-pointer'>Already have an account?<span>Login</span></div>
+            </div>
+        </div>
+    )
+}
+
+export default index;
