a1548141d0a74bd6803503c0e396946b77d1c203
hemant <hk443957@gmail.com>
2023-11-14 02:41:11 +0530

    change in file structure and app run successfully

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const http = require('http');
 const server = http.createServer(app);
 const io = require('socket.io')(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: '*',
         methods: ['GET', 'POST'],
         credentials: true
     }
