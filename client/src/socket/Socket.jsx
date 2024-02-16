0718e16daf6b4c6d3f6ecf0ebfcabf772ea7dea0
hemant <hk443957@gmail.com>
2024-02-16 13:58:49 +0530

    v2-2.message controller folder structure change

--- client/src/socket/Socket.jsx
+++ client/src/socket/Socket.jsx
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const socket = io('https://connectme-backend-9zma.onrender.com', {
+const socket = io('http://localhost:8080', {
     withCredentials: true,
 });
 
--- server/routes/messageRoute.js
+++ server/routes/messageRoute.js
@@ -1,93 +1,10 @@
 const router = require('express').Router();
-const authMiddleware = require('../middleware/authMiddleware');
-const User = require('../models/userModel');
-const Message = require('../models/messageModel');
-const MessageUserList = require('../models/messageUserListModel');
-
+const messageController = require('../Controller/messgeController');
+const authMiddleware = require('../middleware/authMiddleware')
 //sent message to user 
-router.post('/sentMessage', authMiddleware, async (req, res) => {
-    const senderId = req.body.currentUserId;
-    const receiverId = req.body.otherUserId;
-    // console.log(senderId, 'se');
-    // console.log(receiverId, 're');
-    const message = req.body.msg;
-    try {
-        const newMessage = new Message({
-            sender: senderId,
-            receiver: receiverId,
-            messages: [{ content: message }]
-        })
-        await newMessage.save();
-        res.send({
-            success: true,
-            message: "sent successfully"
-        });
-    } catch (error) {
-        res.send(error.message);
-    }
-})
+const messageRoute = router.post('/sentMessage', authMiddleware, messageController.SentMessage)
+    .post('/getMessage', authMiddleware, messageController.GetMessage)
+    .post('/messageUserList', authMiddleware, messageController.MessaageUserList)
 
-//router get all message between sender and receiver 
-router.post('/getMessage', authMiddleware, async (req, res) => {
-    const senderId = req.body.currentUserId;
-    const receiverId = req.body.otherUserId;
-    // console.log(senderId === receiverId);
-    try {
-        const message = await Message.find({
-            $or: [
-                {
-                    $and: [
-                        { sender: senderId },
-                        { receiver: receiverId }
-                    ]
-                },
-                {
-                    $and: [
-                        { sender: receiverId },
-                        { receiver: senderId }
-                    ]
-                }
-            ]
-        }
-        ).sort({ createdAt: 1 })
-        // console.log(message);
-        res.send({
-            data: message,
-            message: "message retrived successfully",
-            success: true
-        })
-    } catch (error) {
-        res.send(error.message);
-    }
-})
-
-//view the list of other user whom current user is massage
-router.post('/messageUserList', authMiddleware, async (req, res) => {
-
-    const receiverId = req.body.otherUserId;
-    const senderId = req.body.currentUserId;
-    try {
-        let senderDocument = await MessageUserList.findOne({ sender: senderId });
-        if (!senderDocument) {
-            senderDocument = new MessageUserList({
-                sender: senderId,
-                receiver: []
-            });
-        }
-        if (!senderDocument.receiver.includes(receiverId)) {
-            senderDocument.receiver.push(receiverId);
-            await senderDocument.save();
-        }
-        await senderDocument.populate('receiver');
-        res.send({
-            success: true,
-            message: "list of user Message",
-            recievers: senderDocument.receiver
-        })
-    } catch (error) {
-        res.send(error.message);
-    }
-})
-
-module.exports = router;
+module.exports = messageRoute;
 
