c8e2dfbffdeb7a18fe43c342a6ac8c58e280212b
hemant <hk443957@gmail.com>
2024-03-06 15:46:35 +0530

    v2.19 message and notification error solved successfully

--- server/Controller/notificationController.js
+++ server/Controller/notificationController.js
@@ -3,11 +3,17 @@ const Notificaiton = require('../models/noficationModel');
 exports.GetNotificationRoute = async (req, res) => {
     try {
         const userId = req.userId;
-        const notification = await Notificaiton.find({ receiver: userId }).populate('sender').sort({ createdAt: -1 });
+        const notification = await Notificaiton.find({
+            receiver: userId,
+            $or: [
+                { action: 'like' },
+                { action: 'comment' }
+            ]
+        }).populate('sender').sort({ createdAt: -1 });
         res.send({
             success: true,
             message: "notication got successfully",
-            data: notification
+            data: notification || []
         })
     } catch (error) {
         res.send(error.message);
