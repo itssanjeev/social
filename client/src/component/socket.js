f2f0abfe0ef08f2013e584c68f1401cd81c80e4d
hemant <hk443957@gmail.com>
2024-04-01 01:30:23 +0530

    v2.34 deploy2

--- client/src/component/socket.js
+++ client/src/component/socket.js
@@ -1,3 +1,2 @@
 import { io } from 'socket.io-client';
 export const socket = io("https://ghost-chat-m4a4.onrender.com", { transports: ['websocket'] });
-// export const socket = io("wss://ghost-chat-m4a4.onrender.com", { transports: ['websocket'] });
