c2b0faf8d718ed5330ac3104efc4a0f1a0842a96
hemant <hk443957@gmail.com>
2024-09-06 21:49:30 +0530

    changed baseurl to remote

--- client/src/apicall/baseUrl.js
+++ client/src/apicall/baseUrl.js
@@ -1,2 +1,2 @@
-export const baseUrl = "http://localhost:8080/";
-// export const baseUrl = "https://ghost-chat-m4a4.onrender.com/"; 
+// export const baseUrl = "http://localhost:8080/";
+export const baseUrl = "https://ghost-chat-m4a4.onrender.com/"; 
